5738d034751e0ca846283d1c2932148a0a70e196
Afif Deshmukh <afifkhanab@gmail.com>
2024-10-18 09:32:15 +0100

    top 150 entries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -109,7 +109,7 @@ public class Main {
                     String queryString = queryParts[2].trim().replace("?","");
 
                     Query parsedQuery = parser.parse(queryString);
-                    TopDocs results = searcher.search(parsedQuery, 50);
+                    TopDocs results = searcher.search(parsedQuery, 150);
 
                     writeResults(writer, idx, results, searcher, similarity);
                 } catch (ParseException | NumberFormatException e) {
